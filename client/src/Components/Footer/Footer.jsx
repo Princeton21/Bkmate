26e99d05d5533b9ee6f86e0f132f11021d4820fe
Sapna Kul <91309280+Sapna127@users.noreply.github.com>
2023-05-01 18:35:06 +0530

    added the footer (#6)
    * book card component created
    
    * footer added
    
    ---------
    
    Co-authored-by: Princeton D'souza <princeton2102@gmail.com>

---
+++ client/src/Components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import classes from './Footer.module.css'
+
+const Footer = () => {
+  return (
+    <footer className={classes.footer}>
+        Created By - 
+        <a href='https://www.linkedin.com/in/princeton-dsouza/'>Princeton</a>
+        <a href='https://www.linkedin.com/in/kritika-choudhary-ba00ab213/'>Kritika</a>
+        <a href='https://www.linkedin.com/in/sonal-gupta-01410b21b/'>Sonal</a>
+        <a href='https://www.linkedin.com/in/sapna-kul-84453a215/'>Sapna</a>
+    </footer>
+  )
+}
+
+export default Footer
