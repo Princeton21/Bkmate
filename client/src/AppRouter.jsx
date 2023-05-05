8aa3a32509ae2979abc59b62527e4dbd9452f42b
Sapna Kul <kulsapna2210002@gmail.com>
2023-05-05 13:46:58 +0530

    created - availableBooks page

--- client/src/AppRouter.jsx
+++ client/src/AppRouter.jsx
@@ -5,12 +5,14 @@ import BookSingle from "./Pages/Book/BookSingle";
 import NotFound from "./Pages/NotFound/NotFound";
 import Profile from "./Pages/Profile/Profile";
 import { Routes, Route } from "react-router-dom";
+import AvailableBooks from "./Pages/AvailableBooks/AvailableBooks";
 const AppRouter = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/post" element={<SellForm />} />
+        {/* <Route path="/availablebooks" element={<AvailableBooks />} /> */}
         <Route path="/books/:bookId" element={<BookSingle />} />
         <Route path="/profile" element={<Profile/>} />
         <Route path="*" element={<NotFound />} />
