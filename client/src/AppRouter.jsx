144bed29fbdeaa88d1e4d3b9a41c2aa46ba29d23
Sapna Kul <kulsapna2210002@gmail.com>
2023-05-05 14:36:53 +0530

    added route for available books page

--- client/src/AppRouter.jsx
+++ client/src/AppRouter.jsx
@@ -5,6 +5,7 @@ import BookSingle from "./Pages/Book/BookSingle";
 import NotFound from "./Pages/NotFound/NotFound";
 import Profile from "./Pages/Profile/Profile";
 import { Routes, Route } from "react-router-dom";
+import AvailableBooks from "./Pages/AvailableBooks/AvailableBooks";
 const AppRouter = () => {
   return (
     <>
@@ -12,6 +13,7 @@ const AppRouter = () => {
         <Route path="/" element={<Home />} />
         <Route path="/post" element={<SellForm />} />
         <Route path="/books/:bookId" element={<BookSingle />} />
+        <Route path="/availablebooks" element={<AvailableBooks />} />
         <Route path="/profile" element={<Profile/>} />
         <Route path="*" element={<NotFound />} />
       </Routes>
