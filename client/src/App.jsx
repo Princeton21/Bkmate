6b053493039b18a0c1eb8e282034fbbdb51cb0c2
Princeton D'souza <princeton2102@gmail.com>
2023-05-07 04:44:04 +0530

    added topscroll

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import AppRouter from "./AppRouter";
 import Footer from "./Components/Footer/Footer";
+import ScrollBar from "./Components/TopScroll/ScrollBar";
 import { BrowserRouter } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
+      <ScrollBar />
       <AppRouter />
       <Footer />
     </BrowserRouter>
